07887c90317557ece9146bbf0714eca02255401e
John Doe <mohamed.benalia@etud.univ-paris8.fr>
2022-10-08 19:47:09 +0200

    Second  commit

--- app/src/main/java/com/example/mymealpartner/MainActivity.java
+++ app/src/main/java/com/example/mymealpartner/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -14,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonStart;
     private boolean startTimer;
     private CountDownTimer foodTimer;
+    private RadioGroup radioGrp;
+    private RadioButton radioBtn;
+
 
 
     @Override
@@ -21,13 +26,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mButtonStart= (Button)findViewById(R.id.startButton);
+        radioGrp=(RadioGroup) findViewById(R.id.radioGroup_time);
+        radioBtn=new RadioButton(this);
 
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.sound);
 
         mButtonStart.setOnClickListener(new View.OnClickListener() {
+            //int radioButtonSelected=radioGrp.getCheckedRadioButtonId();
+            radioBtn=(RadioGroup)findViewById(radioGrp.getCheckedRadioButtonId());
             @Override
             public void onClick(View view) {
                 mButtonStart.setText("STOP");
+
 
                 new CountDownTimer(300000, 10000) {
 
